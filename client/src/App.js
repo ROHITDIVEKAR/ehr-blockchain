ad925b29f4291d86c1fe10db565f7a25c37c391c
Prasun Bhunia <prasunbhunia99@gmail.com>
2023-02-03 18:52:03 +0530

    login & register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,38 @@
-import './App.css';
+import Register from './components/Register';
+import Login from './components/Login';
+import Home from './components/Home';
+import Layout from './components/Layout';
+import Missing from './components/Missing';
+import Unauthorized from './components/Unauthorized';
+import RequireAuth from './components/RequireAuth';
+import { Routes, Route } from 'react-router-dom';
+
+const ROLES = {
+  'User': 2001,
+  'Editor': 1984,
+  'Admin': 5150
+}
 
 function App() {
-    return (
-        <div className="App">
-            Hello World
-        </div>
-    );
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+
+        {/* we want to protect these routes */}
+        <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+
+        {/* catch all */}
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
